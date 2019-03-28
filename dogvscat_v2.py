fee80a89229991a2d6c50817800a4b875089e91e
yanqh2 <187280967@qq.com>
2019-03-28 16:57:38 +0800

    keras

---
+++ dogvscat_v2.py
@@ -0,0 +1,7 @@
+import ptvsd
+
+# Allow other computers to attach to ptvsd at this IP address and port.
+ptvsd.enable_attach(address=('10.122.24.46', 3000), redirect_output=True)
+
+# Pause the program until a remote debugger is attached
+ptvsd.wait_for_attach()
