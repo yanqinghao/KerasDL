3d81d51f3c40d337f75cbae9924faf020b781707
yanqh2 <187280967@qq.com>
2019-03-29 17:41:05 +0800

    dogvscat

--- dogvscat.py
+++ dogvscat.py
@@ -1,5 +1,19 @@
-## data prepare
+import ptvsd
 import os, shutil
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+
+# Allow other computers to attach to ptvsd at this IP address and port.
+ptvsd.enable_attach(address=('10.122.24.46', 3000), redirect_output=True)
+
+# Pause the program until a remote debugger is attached
+ptvsd.wait_for_attach()
+
+## data prepare
+
 # original_dataset_dir = './data/dogs-vs-cats/train'
 base_dir = './data/cats_and_dogs_small'
 # os.mkdir(base_dir)
@@ -59,14 +73,9 @@ test_dogs_dir = os.path.join(test_dir, 'dogs')
 # print('total test cat images:', len(os.listdir(test_cats_dir)))
 # print('total test dog images:', len(os.listdir(test_dogs_dir)))
 
-from keras import layers
-from keras import models
-from keras import optimizers
-from keras.preprocessing.image import ImageDataGenerator
-import matplotlib.pyplot as plt
-
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
@@ -80,22 +89,29 @@ model.add(layers.Dense(1, activation='sigmoid'))
 
 print(model.summary())
 
-model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+model.compile(
+    loss='binary_crossentropy',
+    optimizer=optimizers.RMSprop(lr=1e-4),
+    metrics=['acc'])
 
-train_datagen = ImageDataGenerator(rescale=1./255)
-test_datagen = ImageDataGenerator(rescale=1./255)
-train_generator = train_datagen.flow_from_directory(train_dir, target_size=(150, 150),
-                                                    batch_size=20, class_mode='binary')
-validation_generator = test_datagen.flow_from_directory(validation_dir,target_size=(150, 150),
-                                                        batch_size=20, class_mode='binary')
+train_datagen = ImageDataGenerator(rescale=1. / 255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+train_generator = train_datagen.flow_from_directory(
+    train_dir, target_size=(150, 150), batch_size=20, class_mode='binary')
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir, target_size=(150, 150), batch_size=20, class_mode='binary')
 
 for data_batch, labels_batch in train_generator:
     print('data batch shape:', data_batch.shape)
     print('labels batch shape:', labels_batch.shape)
     break
 
-history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=30,
-                              validation_data=validation_generator, validation_steps=50)
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=50)
 model.save('cats_and_dogs_small_1.h5')
 
 acc = history.history['acc']
@@ -112,6 +128,5 @@ plt.plot(epochs, loss, 'bo', label='Training loss')
 plt.plot(epochs, val_loss, 'b', label='Validation loss')
 plt.title('Training and validation loss')
 plt.legend()
-plt.show()
-
-
+# plt.show()
+plt.savefig('./result/dog_vs_cat_res.png')
--- dogvscat_v2.py
+++ dogvscat_v2.py
@@ -1,7 +1,98 @@
 import ptvsd
+import os
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
 
 # Allow other computers to attach to ptvsd at this IP address and port.
 ptvsd.enable_attach(address=('10.122.24.46', 3000), redirect_output=True)
 
 # Pause the program until a remote debugger is attached
 ptvsd.wait_for_attach()
+
+# data prepare
+# original_dataset_dir = './data/dogs-vs-cats/train'
+base_dir = './data/cats_and_dogs_small'
+# os.mkdir(base_dir)
+train_dir = os.path.join(base_dir, 'train')
+# os.mkdir(train_dir)
+validation_dir = os.path.join(base_dir, 'validation')
+# os.mkdir(validation_dir)
+test_dir = os.path.join(base_dir, 'test')
+# os.mkdir(test_dir)
+train_cats_dir = os.path.join(train_dir, 'cats')
+# os.mkdir(train_cats_dir)
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+# os.mkdir(train_dogs_dir)
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+# os.mkdir(validation_cats_dir)
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+# os.mkdir(validation_dogs_dir)
+test_cats_dir = os.path.join(test_dir, 'cats')
+# os.mkdir(test_cats_dir)
+test_dogs_dir = os.path.join(test_dir, 'dogs')
+
+model = models.Sequential()
+model.add(
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+print(model.summary())
+
+model.compile(
+    loss='binary_crossentropy',
+    optimizer=optimizers.RMSprop(lr=1e-4),
+    metrics=['acc'])
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+train_generator = train_datagen.flow_from_directory(
+    train_dir, target_size=(150, 150), batch_size=32, class_mode='binary')
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir, target_size=(150, 150), batch_size=32, class_mode='binary')
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=100,
+    validation_data=validation_generator,
+    validation_steps=50)
+
+model.save('cats_and_dogs_small_2.h5')
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+# plt.show()
+plt.savefig('./result/dog_vs_cat_res_2.png')
